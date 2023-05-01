017de691e30eb909a2a7fe1fbd0e6a643e71ddfe
VichetN <vichet.noeng66@gmail.com>
2023-05-01 16:23:52 +0700

    update

--- src/components/accountMapping/balanceSheet.js
+++ src/components/accountMapping/balanceSheet.js
@@ -1,75 +1,92 @@
 "use client";
 
-import { useDrop } from "react-dnd";
 import cx from "classnames";
-import { ItemTypes, accountingTypes } from "@/constants";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { groupTypeAtom, parsedDataAtom } from "@/recoils";
 import { Draggable, Droppable } from "react-beautiful-dnd";
+import { Fragment } from "react";
 
-function DroppableType({ subType = [], handleUpdateType }) {
-  const parsedData = useRecoilValue(parsedDataAtom);
-  const [{ isActive }, drop] = useDrop(() => ({
-    accept: ItemTypes.ROW,
-    collect: (monitor) => ({
-      isActive: monitor.canDrop() && monitor.isOver(),
-    }),
-    drop: (_item, monitor) => {
-      const didDrop = monitor.didDrop();
-      if (didDrop) {
-        return;
-      }
-      handleUpdateType(parsedData?.data, _item?.rowIndex, subType?.title);
-    },
-  }));
-  return (
-    <li
-      ref={drop}
-      key={subType?.id}
-      className={cx(
-        "p-2 bg-gray-200 rounded-lg my-2 transition-all ease-in duration-150",
-        {
-          "outline outline-indigo-400": isActive,
-        }
-      )}
-    >
-      {subType?.title}
-    </li>
-  );
-}
+// function DroppableType({ subType = [], handleUpdateType }) {
+//   const parsedData = useRecoilValue(parsedDataAtom);
+//   const [{ isActive }, drop] = useDrop(() => ({
+//     accept: ItemTypes.ROW,
+//     collect: (monitor) => ({
+//       isActive: monitor.canDrop() && monitor.isOver(),
+//     }),
+//     drop: (_item, monitor) => {
+//       const didDrop = monitor.didDrop();
+//       if (didDrop) {
+//         return;
+//       }
+//       handleUpdateType(parsedData?.data, _item?.rowIndex, subType?.title);
+//     },
+//   }));
+//   return (
+//     <li
+//       ref={drop}
+//       key={subType?.id}
+//       className={cx(
+//         "p-2 bg-gray-200 rounded-lg my-2 transition-all ease-in duration-150",
+//         {
+//           "outline outline-indigo-400": isActive,
+//         }
+//       )}
+//     >
+//       {subType?.title}
+//     </li>
+//   );
+// }
 
-function Row({ rowData, index }) {
+function Row({ rowData }) {
   return (
-    <>
-      <tr>
-        <td className="py-2">
-          {rowData?.title}
-          <ul>
-            {rowData?.sub?.map((load, i) => (
-              <Draggable
-                draggableId={`balanceSheet-${rowData?.id}-${load?.id}`}
-                index={i}
-                key={load?.id}
+    <td className="py-2">
+      <b>{rowData?.title}</b>
+      <div className="pl-4 ">
+        {rowData?.sub?.map((load, i) => (
+          <Draggable
+            draggableId={`balanceSheet-${rowData?.id}-${load?.id}`}
+            index={i}
+            key={`${rowData?.id}-${load?.id}`}
+          >
+            {(provided, snapshot) => (
+              <div
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
               >
-                {(provided, snapshot) => (
-                  <li
-                    key={`${rowData?.id}-${load?.id}`}
-                    className={cx("pl-4 py-2", {
-                      "bg-indigo-200/60": snapshot.isDragging,
+                <div
+                  key={`${rowData?.id}-${load?.id}`}
+                  className={cx("px-4 py-2 rounded-sm", {
+                    "bg-indigo-200/60": snapshot.isDragging,
+                  })}
+                >
+                  {load?.title}
+                </div>
+
+                {[...Array(3)].map((_, index) => (
+                  <div
+                    className={cx("pl-8 w-full flex", {
+                      // hidden: snapshot.isDragging,
                     })}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
+                    key={index}
                   >
-                    {load?.title}
-                  </li>
-                )}
-              </Draggable>
-            ))}
-          </ul>
-        </td>
-      </tr>
-    </>
+                    <div className="px-3 flex-1 border">Salary</div>
+                    <div className="w-[400px] grid grid-cols-2">
+                      <div className="px-3 border">
+                        {Number(400).toFixed(2)}
+                      </div>
+                      <div className="px-3 border">
+                        {Number(420).toFixed(2)}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </Draggable>
+        ))}
+      </div>
+    </td>
   );
 }
 
@@ -95,22 +112,27 @@ function BalanceSheet() {
       <div>
         <h2 className="uppercase font-bold">Balance Sheet</h2>
       </div>
-      <Droppable droppableId="balanceSheet" type="accountTable">
-        {(provided, snapshot) => (
-          <table
-            className="rounded-lg"
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <tbody>
-              {groupType?.map((r, i) => (
-                <Row key={r?.id} index={i} rowData={r} />
-              ))}
-              {provided.placeholder}
-            </tbody>
-          </table>
-        )}
-      </Droppable>
+
+      <table className="w-full rounded-lg">
+        <tbody>
+          {groupType?.map((r, i) => (
+            <Droppable
+              droppableId={`balanceSheet-id-${r?.id}`}
+              type="accountTable"
+            >
+              {(provided, snapshot) => (
+                <Fragment key={r?.id}>
+                  <tr ref={provided.innerRef} {...provided.droppableProps}>
+                    <Row index={i} rowData={r} />
+                  </tr>
+                  {provided.placeholder}
+                </Fragment>
+              )}
+            </Droppable>
+          ))}
+        </tbody>
+      </table>
+
       {/* <div ref={provided.innerRef} {...provided.droppableProps}>
             {groupType?.map((load) => (
               <div key={load.id}>
